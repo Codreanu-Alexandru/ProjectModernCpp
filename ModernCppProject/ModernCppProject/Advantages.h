734fcf6e0756e94fcf6d21d2702463e258b76a72
AndreiCirstea <andreicirstea2@gmail.com>
2022-11-12 15:20:15 +0200

    Created header for class Advantages

---
+++ ModernCppProject/ModernCppProject/Advantages.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+class Advantages
+{
+public:
+	Advantages();
+	void applyFiftyFifty();
+	void applyChooseAnswer();
+	void applySuggestAnswer();
+
+private:
+	bool m_fiftyFiftyAvailable;
+	bool m_chooseAnswerAvailable;
+	bool m_suggestAnswerAvailable;
+
+
+	
+};
+
