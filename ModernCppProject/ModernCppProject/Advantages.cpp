0f29adf9cfda741ed8bada0766e00ebf498391a7
AndreiCirstea <andreicirstea2@gmail.com>
2022-11-19 14:33:48 +0200

    Created displayAdvantages method with a selection menu for the advantages available

--- ModernCppProject/ModernCppProject/Advantages.cpp
+++ ModernCppProject/ModernCppProject/Advantages.cpp
@@ -9,15 +9,80 @@ Advantages::Advantages()
 
 void Advantages::applyFiftyFifty()
 {
-	m_fiftyFiftyAvailable = false;
+	if (m_fiftyFiftyAvailable)
+	{
+
+		m_fiftyFiftyAvailable = false;
+	}
+	
 }
 
 void Advantages::applyChooseAnswer()
 {
-	m_chooseAnswerAvailable = false;
+	if (m_chooseAnswerAvailable)
+	{
+		m_chooseAnswerAvailable = false;
+	}
+	
 }
 
 void Advantages::applySuggestAnswer()
 {
-	m_suggestAnswerAvailable = false;
+	if (m_suggestAnswerAvailable)
+	{
+		m_suggestAnswerAvailable = false;
+	}
+	
+}
+
+void Advantages::displayAdvantages(Question question)
+{
+	uint8_t option;
+	SingleNumericQuestion* a = dynamic_cast<SingleNumericQuestion*>(&question);
+	std::cout << "Avantaje disponibile: \n";
+	if (a!=nullptr)
+	{
+		if (m_fiftyFiftyAvailable)
+			std::cout << "7: 50/50 - Nu e valabil pentru acest tip de intrebare \n";
+		if (m_chooseAnswerAvailable)
+			std::cout << "8: Alege intre 4 raspunsuri \n";
+		if (m_suggestAnswerAvailable)
+			std::cout << "9: Genereaza un raspuns apropiat \n";
+
+		std::cin >> option;
+		switch (option)
+		{
+		case 8:
+			applyChooseAnswer();
+			break;
+		case 9:
+			applySuggestAnswer();
+			break;
+		default:
+			break;
+		}
+	}
+	else
+	{
+		if (m_fiftyFiftyAvailable)
+			std::cout << "7: 50/50 \n";
+		if (m_chooseAnswerAvailable)
+			std::cout << "8: Alege intre 4 raspunsuri - Nu este valabil pentru acest tip de intrebare \n";
+		if (m_suggestAnswerAvailable)
+			std::cout << "9: Sugereaza raspunsul corect \n";
+		std::cin >> option;
+		switch (option)
+		{
+		case 7:
+			applyFiftyFifty();
+			break;
+		case 9:
+			applySuggestAnswer();
+			break;
+		default:
+			break;
+		}
+	}
+	
+
 }
--- ModernCppProject/ModernCppProject/Advantages.h
+++ ModernCppProject/ModernCppProject/Advantages.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include "MultipleChoiceQuestion.h"
+#include "SingleNumericQuestion.h"
 
 class Advantages
 {
@@ -8,6 +10,8 @@ public:
 	void applyFiftyFifty();
 	void applyChooseAnswer();
 	void applySuggestAnswer();
+	void displayAdvantages(Question question);
+
 
 private:
 	bool m_fiftyFiftyAvailable;
