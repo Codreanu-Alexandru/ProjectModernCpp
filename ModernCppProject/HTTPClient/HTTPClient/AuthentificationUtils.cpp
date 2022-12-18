5b7c0def241c2dd96f3996a999c9cb0a4943eeed
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2022-12-18 17:09:50 +0200

    Updated AuthentificationUtils source file.

--- ModernCppProject/HTTPClient/HTTPClient/AuthentificationUtils.cpp
+++ ModernCppProject/HTTPClient/HTTPClient/AuthentificationUtils.cpp
@@ -1,6 +1,6 @@
 #include "AuthentificationUtils.h"
 
-cpr::Response response = cpr::Get(cpr::Url{ "http://localhost:4960/users" });
+cpr::Response response = cpr::Get(cpr::Url{ "http://localhost:18080/users" });
 
 bool correctAuthentication(std::string username, std::string password) {
 
