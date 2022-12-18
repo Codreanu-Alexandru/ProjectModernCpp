284465100d0d1c86901c439ee0f7134bca20d528
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2022-12-18 17:08:50 +0200

    Main source file.

---
+++ ModernCppProject/HTTPClient/GUI/main.cpp
@@ -0,0 +1,25 @@
+#include "GUI.h"
+#include <QtWidgets/QApplication>
+#include <qfile.h>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <regex>
+#include <string>
+#include <cpr/cpr.h>
+#include <crow.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //Setting the template
+    QFile styleSheetFile("./Irrorater.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet(styleSheet);
+
+    GUI w;
+    w.show();
+    return a.exec();
+}
