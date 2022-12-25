9dda2fe53b720786bdf724506cc5584516bc1d74
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2022-12-25 18:59:01 +0200

    Modified function syntaxes for log in functionality.

---
+++ ModernCppProject/HTTPClient/GUI/AuthUtils.cpp
@@ -0,0 +1,43 @@
+#include "AuthUtils.h"
+
+bool correctAuthentication(std::string username, std::string password, cpr::Response response) {
+
+    auto users = crow::json::load(response.text);
+    for (const auto& user : users) {
+
+        if (user["username"].s() == username) {
+
+            if (user["password"].s() == password) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool existingUser(std::string username, cpr::Response response) {
+
+    auto users = crow::json::load(response.text);
+    for (const auto& user : users) {
+
+        if (user["username"].s() == username) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void showUsers(cpr::Response response)
+{
+    std::cout << "Here is the list of Users(just for now):\n";
+
+    auto users = crow::json::load(response.text);
+    for (const auto& user : users) {
+        std::cout << user["id"] << ' '
+            << user["username"].s() << ' '
+            << user["password"] << ' '
+            << user["matchHistory"] << '\n';
+    }
+}
---
+++ ModernCppProject/HTTPClient/GUI/AuthUtils.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <regex>
+#include <string>
+#include <cpr/cpr.h>
+#include <crow.h>
+
+bool correctAuthentication(std::string username, std::string password, cpr::Response response);
+bool existingUser(std::string username, cpr::Response response);
+void showUsers(cpr::Response response);
