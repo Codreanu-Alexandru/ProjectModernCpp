69cfb0d9217f149f7c91de3fcfaaf2158412fe02
CirsteaAndrei <andreicirstea2@gmail.com>
2023-01-12 22:43:15 +0200

    Implemented the client Game window so that, in the constructor, it receives the map dimensions from the server and generates it as a grid layout of buttons in a new qwidget

--- ModernCppProject/HTTPClient/GUI/Game.cpp
+++ ModernCppProject/HTTPClient/GUI/Game.cpp
@@ -1,11 +1,47 @@
 #include "Game.h"
 
-Game::Game(QWidget *parent)
+Game::Game(QWidget *parent, std::string username)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 	parentWindow = parent;
+	this->username = username;
+	int mapHeight = 0;
+	int mapWidth = 0;
+	auto startGame = cpr::Put(
+		cpr::Url{ "http://localhost:4960/startGame" }
+	);
+	if (startGame.status_code == 200 || startGame.status_code == 201) {
+		auto gameData = crow::json::load(startGame.text);
+		mapHeight = gameData["mapHeight"].i();
+		mapWidth = gameData["mapWidth"].i();
+		map = new QGridLayout;
+		generateMap(mapHeight, mapWidth);
+		showMap(mapHeight, mapWidth);
+	}
 }
 
 Game::~Game()
 {}
+
+void Game::generateMap(size_t rows, size_t cols)
+{
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			QPushButton* button = new QPushButton("r");
+			button->resize(40, 40);
+			button->move(40 * j, 40 * i);
+			button->show();
+			map->addWidget(button, i, j);
+		}
+	}
+	mapWidget = new QWidget;
+	mapWidget->setLayout(map);
+}
+
+void Game::showMap(size_t kHeight, size_t kWidth)
+{
+	QMainWindow *window = new QMainWindow;
+	this->setCentralWidget(mapWidget);
+	update();
+}
--- ModernCppProject/HTTPClient/GUI/Game.h
+++ ModernCppProject/HTTPClient/GUI/Game.h
@@ -2,16 +2,30 @@
 
 #include <QMainWindow>
 #include "ui_Game.h"
+#include <cpr/cpr.h>
+#include <crow.h>
+#include "qgridlayout.h"
+#include "qpushbutton.h"
+#include "qwidget.h"
+#include "qstring.h"
 
 class Game : public QMainWindow
 {
 	Q_OBJECT
 
 public:
-	Game(QWidget *parent = nullptr);
+	Game(QWidget *parent, std::string username);
 	~Game();
+	
+	void generateMap(size_t kHeight, size_t kWidth);
+	void showMap(size_t kHeight, size_t kWidth);
 
 private:
 	Ui::GameClass ui;
 	QWidget *parentWindow;
+	int mapWidth;
+	int mapHeight;
+	std::string username;
+	QGridLayout* map;
+	QWidget* mapWidget;
 };
