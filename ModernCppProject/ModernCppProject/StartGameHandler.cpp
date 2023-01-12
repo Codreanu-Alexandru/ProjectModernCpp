299b78630a0c241fa5d32458a9bcb372beab5835
CirsteaAndrei <andreicirstea2@gmail.com>
2023-01-12 22:32:45 +0200

    Created StartGameHandler class that receives the message that the game can start from the client and sends back the data needed for the display of the map.

---
+++ ModernCppProject/ModernCppProject/StartGameHandler.cpp
@@ -0,0 +1,23 @@
+#include "StartGameHandler.h"
+
+StartGameHandler::StartGameHandler(Game *game, Lobby& lobby)
+{
+	this->lobby = &lobby;
+	this->game = game;
+}
+
+crow::response StartGameHandler::operator()(const crow::request& req) const
+{
+	if (game->running == false) {
+		std::vector<Player> players = lobby->getPlayers();
+		game = new Game(players);
+	}
+	crow::json::wvalue gameData;
+	int mapHeight = static_cast<int>(game->getMap()->GetHeight());
+	std::cout << mapHeight << std::endl;
+	int mapWidth = static_cast<int>(game->getMap()->GetWidth());
+	gameData["mapHeight"] = mapHeight;
+	gameData["mapWidth"] = mapWidth;
+
+	return crow::response(200, gameData);
+}
---
+++ ModernCppProject/ModernCppProject/StartGameHandler.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Game.h"
+#include "Lobby.h"
+#include"Player.h"
+#include <vector>
+
+class StartGameHandler
+{
+public:
+	StartGameHandler(Game* game, Lobby &lobby);
+
+	crow::response operator() (const crow::request& req) const;
+private:
+	mutable Game* game;
+	mutable Lobby* lobby;
+};
+
